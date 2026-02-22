38bf691d381c405bba23d90354e6bfdd11d6676a
AbdullahAlHadabi <Abdullah.Juma280@hotmail.com>
2026-02-22 10:39:34 +0400

    MULTIPLICATION & DIVISION

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
+        // =========== Multiplication & Division =============
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("--- MULTIPLICATION ---");
+
+        System.out.println("Enter the first number");
+        int num90= sc.nextInt();
+
+        System.out.println("Enter the secound number");
+        int num80=sc.nextInt();
+
+        System.out.println(" The product of the first number and secound number is :  " +num90*num80);
+
+
+        // ================= DIVISION =================
+
+        System.out.println("\n--- DIVISION ---");
+        System.out.print("Enter first number: ");
+        int num40 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num50 = sc.nextInt();
+
+        if (num50 == 0) {
+            System.out.println("Error: Cannot divide by zero.");
+        } else {
+            double result = (double) num40 / num50;
+            System.out.println("✅ " + num40 + " ÷ " + num50 + " = " + result);
         }
+
+        sc.close();
+
+
     }
 }
