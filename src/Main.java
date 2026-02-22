36803f9672a709d6f5499703aa408cc53d846be2
SulaimanAlfarsi <sulaimanalfarsi3060@gmail.com>
2026-02-22 11:07:58 +0400

    Add Power (Math.pow) and Square Root features

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Sulaiman
+
+        Scanner sc = new Scanner(System.in);
+
+        // POWER
+        System.out.println("\n--- POWER ---");
+
+        System.out.print("Enter base: ");
+        int base = sc.nextInt();
+
+        System.out.print("Enter exponent: ");
+        int exponent = sc.nextInt();
+
+        int result = 1;
+
+        for (int i = 0; i < exponent; i++) {
+            result = result * base;
         }
+
+        System.out.println( base + " ^ " + exponent + " = " + result);
+
+        // SQUARE ROOT
+        System.out.println("\n--- SQUARE ROOT ---");
+
+        System.out.print("Enter a number: ");
+        double number = sc.nextDouble();
+
+        if (number < 0) {
+            System.out.println("Error: Cannot compute square root of a negative number.");
+            return;
+        }
+
+        double guess = 0.0;
+        double increment = 0.001;
+
+        while (guess * guess <= number) {
+            guess += increment;
+        }
+
+        guess -= increment;
+
+        System.out.println(number + " ≈ " + guess);
     }
 }
