7bffc9c1a98d8b6e128c05f1fead059ec5b72a71
abdulmajeedalbalushi <codeline@example.com>
2026-02-22 11:42:35 +0400

    adition and sub

--- src/Main.java
+++ src/Main.java
@@ -34,6 +34,22 @@ public class Main {
             System.out.println("✅ " + num40 + " ÷ " + num50 + " = " + result);
         }
 
+        // Addition & subtraction
+        Scanner inputBufferObj = new Scanner(System.in);
+        int addition = 0;
+        int subtraction = 0;
+
+        System.out.println("please enter 2 numbers: ");
+        int firstNumber = inputBufferObj.nextInt();
+        int secondNumber = inputBufferObj.nextInt();
+
+        addition = firstNumber + secondNumber;
+        subtraction = firstNumber - secondNumber;
+
+        System.out.printf("the sum of %s and %s is %s", firstNumber , secondNumber, addition);
+        System.out.printf("\nthe differene of %d and %d is %s", firstNumber, secondNumber, subtraction);
+
+
         sc.close();
 
 
