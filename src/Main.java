d6b55418b20833e48e6a8fa9935920da5515adb2
abbhosni <binkhalidabdulahv@gmail.com>
2026-02-22 12:29:29 +0400

    staircase and Multiplication function done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+        }
+
+        //Number Staircase function.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- NUMBER STAIRCASE PATTERN ---");
+        System.out.print("Enter number of rows: ");
+        int indexStaircase = Integer.parseInt(sc.nextLine());
+        if (indexStaircase>=0){
+
+            //upper indexes
+            ArrayList<Integer> ladder = new ArrayList<Integer>();
+            for (int column = 1;column<=indexStaircase;column++){
+                ladder.add(column);
+                System.out.println(ladder);
+            }
+        }
+        else {
+            System.out.println("Error: Number of rows must be a positive integer.");
+        }
+
+        //Multiplication table function
+        System.out.println("--- MULTIPLICATION TABLE ---");
+        System.out.print("Enter a number : ");
+        int indexMultiplication = Integer.parseInt(sc.nextLine());
+        for (int count = 1 ;count<=indexMultiplication;count++){
+            int Process = indexMultiplication * count;
+            System.out.printf("%d   x   %d  = %d\n",indexMultiplication, count, Process );
         }
     }
 }
